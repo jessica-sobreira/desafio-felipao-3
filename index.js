1c2da13b95cefcfa62c006e602d5e561391d27a0
jessica-sobreira <j.jecristina@gmail.com>
2023-11-11 21:07:30 -0300

    adicionar personagens

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+class heroi {
+    constructor(nome, idade, tipo, ataque) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.ataque = ataque;
+    }
+
+    atacar() {
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+
+
+}
+
+let mago = new heroi ('Galdalff', 105, 'Mago', 'Magia');
+mago.atacar();
+
+let guerreiro = new heroi ('He-man', 25, 'Guerreiro', 'Espada');
+guerreiro.atacar();
+
+let monge = new heroi ('Bruce Lee', 30, 'Monge', 'Artes Marciais');
+monge.atacar();
+
+let ninja = new heroi ('Naruto', 30, 'Ninja', 'Shuriken');
+ninja.atacar();
